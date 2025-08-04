464bc48f5317fb811ce8469b2cc4c5efdb6ccc12
Haya <haya.ehab.dev@gmail.com>
2025-08-04 04:00:53 +0300

    initial commits

---
+++ TypeConver.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+int main() {
+
+
+// type coversion = conversion value of one data to another
+// implicit = automatic
+//Explicit = Manual
+
+
+// implicit conversion
+// int correct = 8;
+// int questions = 10;
+// double score = correct/questions * 100;
+
+// Explicit conversion
+// int correct = 8;
+// int questions = 10;
+// double score = correct/(double)questions * 100;
+
+
+return 0;
+}
